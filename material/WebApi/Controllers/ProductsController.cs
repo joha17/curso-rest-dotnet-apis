2584ed9c61635d880b62d9c905828e411390822a
Johannes <cervapz@gmail.com>
2020-07-23 20:56:43 -0600

    .

--- material/WebApi/Controllers/ProductsController.cs
+++ material/WebApi/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -48,10 +49,26 @@ namespace WebApi.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] string name)
+        public IActionResult Create([FromBody] string name)
         {
             _repository.Save(name);
-            return Ok();
+
+            var items = _repository.Get();
+            dynamic value = items.Last();
+            return CreatedAtAction(nameof(GetById), new { Id= value.Id }, value);
         }
+
+        [HttpPut("id")]
+        public IActionResult Put(int id)
+        {
+            return new ObjectResult(new object()) { StatusCode = (int)HttpStatusCode.NotImplemented};
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
+        {
+            return new ObjectResult(new object()) { StatusCode = (int)HttpStatusCode.NotImplemented };
+        }
+
     }
 }
