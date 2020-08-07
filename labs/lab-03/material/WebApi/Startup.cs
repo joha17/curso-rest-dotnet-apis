d61232264e9fd5343d898ca01ecb49bcb0c6d0b2
Johannes <cervapz@gmail.com>
2020-08-06 21:54:18 -0600

    Se agrega el proyecto al laboratorio 3

---
+++ labs/lab-03/material/WebApi/Startup.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using WebApi.Controllers;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            ApplicationEnvironment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment ApplicationEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //InCode
+            //AppSettings
+            //Environment Variable
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<ApplicationSettings>(appSettingsSection);
+
+            services.AddSingleton<ApplicationSettings>(new ApplicationSettings { 
+                Variable = System.Environment.GetEnvironmentVariable("Variable")  ?? "123"}
+            );
+
+            services.AddSingleton<ProductRepository>();
+
+            services.AddCors(options => options.AddDefaultPolicy(builder => {
+                
+                // Fluent API
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseCors();
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
